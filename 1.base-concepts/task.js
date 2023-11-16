38c4867331be4eb47fb36a2143e8d22a2bc42ca1
DrAVl <DrAV432@mail.ru>
2023-11-16 12:37:21 +0700

    code task 1_4

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,31 +1,32 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let x1;
-let x2;
-let d = b ** 2 - 4 * a * c;
-  if (d > 0) {
-    x1 = (-b + Math.sqrt(d)) / (2 * a);
-    x2 = (-b - Math.sqrt(d)) / (2 * a);
-    arr.push(x1, x2);
-  } else if (d === 0) {
-    x1 = -b / (2 * a);
-    arr.push(x1);
-  }
-  return arr;
+	let arr = [];
+	let x1;
+	let x2;
+	let d = b ** 2 - 4 * a * c;
+	if (d > 0) {
+		x1 = (-b + Math.sqrt(d)) / (2 * a);
+		x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(x1, x2);
+	} else if (d === 0) {
+		x1 = -b / (2 * a);
+		arr.push(x1);
+	}
+	return arr;
 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let credit;
-  let payment;
-  let amountOfCredit;
-   if (percent<0) {
-    return false
-  }
-  percent = percent/100/12
-credit=amount-contribution
-payment=credit * (percent + (percent / (((1 + percent) ** countMonths) -1)))
-amountOfCredit = payment * countMonths
-return parseFloat (amountOfCredit.toFixed(2));
+	let credit;
+	let payment;
+	let amountOfCredit;
+	if (percent < 0) {
+		return false
+	}
+	percent = percent / 100 / 12
+	credit = amount - contribution
+	payment = credit * (percent + (percent / (((1 + percent) ** countMonths) - 1)))
+	amountOfCredit = payment * countMonths
+	return parseFloat(amountOfCredit.toFixed(2));
 }
--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,25 @@
 function getArrayParams(...arr) {
+if (arr.length === 0) {
+  return {
+    min: null,
+    max: null,
+    avg null
+  };
+}
+let min = arr[0];
+let max = arr[0];
+let avg = arr[0];
+
+for (let i = 1; i < arr.lenght; i++) {
+  if arr[i] < min {
+    min = arr[i];
+  }
+  if arr[i] > max {
+    max = arr[i];
+  }
+  sum += arr[i]
+}
+avg = sum/arr.length;
 
   return { min: min, max: max, avg: avg };
 }
