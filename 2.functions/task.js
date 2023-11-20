6fd1eb91cb5d49b980b8bfeb9073c1dc242189b5
DrAVl <DrAV432@mail.ru>
2023-11-20 16:05:02 +0700

    code task 2_1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,27 +1,39 @@
 function getArrayParams(...arr) {
-if (arr.length === 0) {
-  return {
-    min: null,
-    max: null,
-    avg null
-  };
-}
-let min = arr[0];
-let max = arr[0];
-let avg = arr[0];
+	if (arr.length === 0) {
+		return {
+			min: null,
+			max: null,
+			avg: null
+		};
+	};
+	let min = 0; //= arr[0];
+  let max = 0; //= arr[0];
+	let summa = 0;// = arr[0];
+  let avg = 0;
+let initialValue = 0
+	 max = Math.max(...arr);
+	 min = Math.min(...arr);
 
-for (let i = 1; i < arr.lenght; i++) {
-  if arr[i] < min {
-    min = arr[i];
-  }
-  if arr[i] > max {
-    max = arr[i];
-  }
-  sum += arr[i]
-}
-avg = sum/arr.length;
+//	for (let i = 0; i < arr.lenght; i++) {
+		// if (arr[i] < min) {
+		//   min = arr[i];
+		// }
+		// if (arr[i] > max) {
+		//   max = arr[i];
+		//}
+//		summa += arr[i];
+	//};
+	//const sum = arr.reduce((acc, number) => acc + number, 0);
+	summa = arr.reduce(
+	 (accumulator, currentValue) => accumulator + currentValue,
+	  initialValue,);
+	avg = summa / arr.length;
 
-  return { min: min, max: max, avg: avg };
+	return {
+		min: min,
+		max: max,
+		avg: Number(avg.toFixed(2))
+	};
 }
 
 function summElementsWorker(...arr) {
@@ -40,6 +52,5 @@ function averageEvenElementsWorker(...arr) {
 
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfArr, func) {
 }
