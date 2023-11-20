93db1d540bc77eb8636432bdd1dfc796e533842f
DrAVl <DrAV432@mail.ru>
2023-11-20 16:47:26 +0700

    code task 2_1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -6,29 +6,17 @@ function getArrayParams(...arr) {
 			avg: null
 		};
 	};
-	let min = 0; //= arr[0];
-  let max = 0; //= arr[0];
-	let summa = 0;// = arr[0];
-  let avg = 0;
-let initialValue = 0
-	 max = Math.max(...arr);
-	 min = Math.min(...arr);
-
-//	for (let i = 0; i < arr.lenght; i++) {
-		// if (arr[i] < min) {
-		//   min = arr[i];
-		// }
-		// if (arr[i] > max) {
-		//   max = arr[i];
-		//}
-//		summa += arr[i];
-	//};
-	//const sum = arr.reduce((acc, number) => acc + number, 0);
+	let min = 0;
+	let max = 0;
+	let summa = 0;
+	let avg = 0;
+	let initialValue = 0
+	max = Math.max(...arr);
+	min = Math.min(...arr);
 	summa = arr.reduce(
-	 (accumulator, currentValue) => accumulator + currentValue,
-	  initialValue,);
+		(accumulator, currentValue) => accumulator + currentValue,
+		initialValue, );
 	avg = summa / arr.length;
-
 	return {
 		min: min,
 		max: max,
@@ -52,5 +40,4 @@ function averageEvenElementsWorker(...arr) {
 
 }
 
-function makeWork(arrOfArr, func) {
-}
+function makeWork(arrOfArr, func) {}
